7e59b19ee89b8f4495942d0f96c87a49d86ac905
wyf-cyber <wyf17326280236@gmail.com>
2023-12-27 15:38:13 +0800

    node

--- homework/vivado实验/CPU.v
+++ homework/vivado实验/CPU.v
@@ -18,11 +18,40 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-//000000b3 00108093 00000133 00210113 000001b3 00318193 001020a3 00202123 003021a3 00102203 00202283 00302303;
-`define WDSel_FromALU 2'b00
-`define WDSel_FromMEM 2'b01
 
-module SCPU_TOP(
+//beq_test code:00332193 00632093 00732113 ffe32213 fe208ae3 fe3088e3 00528033
+//slti x3,x6,3
+//label:
+//slti x1,x6,6
+//slti x2,x6,7
+//slti x4,x6,-2
+//beq x1,x2,label
+//beq x1,x3,label
+//add x0,x5,x5
+
+//jal_test code:00332193 00632093 00732113 ffe32213 ff5ff46f 00528033
+//slti x3,x6,3
+//label:
+//slti x1,x6,6
+//slti x2,x6,7
+//slti x4,x6,-2
+//jal x8,label
+//add x0,x5,x5
+
+//jalr_test code:00332193 00000293 00428467 00632093 00732113 ffe32213 00528033
+//slti x3,x6,3
+//addi x5,x0,0
+//jalr x8,4(x5)
+//slti x1,x6,6
+//slti x2,x6,7
+//slti x4,x6,-2
+//add x0,x5,x5
+
+
+`define MemtoReg_FromALU 2'b00
+`define MemtoReg_FromMEM 2'b01
+`define MemtoReg_FromPC 2'b10
+module CPU(
     input clk,  //100MHZ CLK
     input rstn,  //reset signal
     input [15:0] sw_i, //sw_i[15]---sw_i[0] 
@@ -40,49 +69,56 @@ module SCPU_TOP(
     //sw_i[15]选择慢速模式或快速模式
     assign Clk_CPU = (sw_i[15])? clkdiv[27] : clkdiv[25];
     
-    reg[4:0]rom_addr;
+    reg[4:0]rom_addr = 5'b11111;
     wire[31:0] inst_in;
-
+    wire[2:0] branch;
     //rom每周期地址顺序取数
-    always @(posedge Clk_CPU or negedge rstn) begin
-        if(!rstn) begin rom_addr = 5'b0; end
-        else begin
-            if (rom_addr < 12) begin
-                if(sw_i[1]==1'b0)begin
-                    rom_addr = rom_addr + 1'b1;
-                end
-            end
-            else  
-                rom_addr = 5'b00000; 
-            end
-        end
-
+    //always @(posedge Clk_CPU or negedge rstn) begin
+    //    if(!rstn) begin rom_addr = 5'b0; end
+    //    else begin
+    //        if (rom_addr < 16 & rom_addr >=0) begin
+    //            if(sw_i[1]==1'b0 & branch[0]==1'b0)begin
+    //                rom_addr = rom_addr + 1'b1;
+    //            end else begin 
+    //                rom_addr = rom_addr;
+    //               // branch_reg[0] = 1'b0;
+    //                end
+    //        end
+    //        else
+    //            rom_addr = 5'b00000; 
+    //        end
+    //    end
+ 
     //从coe文件中取指令
-    dist_mem_gen_cz2 u_im(
+    dist_instr u_im(
         .a(rom_addr),
         .spo(inst_in)
     );
     
     reg[6:0] Op;
-    reg[6:0] Funct7;
-    reg[2:0] Funct3;
+    reg[6:0] fun7;
+    reg[2:0] fun3;
     reg[11:0] iimm;
     reg[11:0] simm;
-    wire[1:0] WDSel;
+    reg[19:0] jimm;
+    reg[11:0] bimm;
+    wire[1:0] MemtoReg;
     wire[5:0] EXTOp;
     reg[4:0]rs1;
     reg[4:0]rs2;
     reg[4:0]rd;
     //Decode
     always@(*)begin
-        Op = inst_in[6:0];  // op
-        Funct7 = inst_in[31:25]; // funct7
-        Funct3 = inst_in[14:12]; // funct3
+        opcode = inst_in[6:0];  // opcode
+        fun7 = inst_in[31:25]; // fun7
+        fun3 = inst_in[14:12]; // fun3
         rs1 = inst_in[19:15];  // rs1
         rs2 = inst_in[24:20];  // rs2
         rd = inst_in[11:7];  // rd
         iimm=inst_in[31:20];//addi 指令立即数，lw指令立即数
-        simm={inst_in[31:25],inst_in[11:7]}; //sw指令立即数  
+        simm={inst_in[31:25],inst_in[11:7]}; //sw指令立即数 
+        jimm={inst_in[31],inst_in[19:12],inst_in[20],inst_in[30:21]};  //jal指令立即数
+        bimm={inst_in[31],inst_in[7],inst_in[30:25],inst_in[11:8]};   //beq指令立即数
     end
     
     wire ALUSrc;
@@ -94,24 +130,27 @@ module SCPU_TOP(
     wire [2:0] DMType;
     wire MemWrite;
     wire[31:0] 	immout;
+
     //生成控制信号
-    ctrl u_ctrl(
-        .Op(Op),  //opcode
-        .Funct7(Funct7),  //funct7 
-        .Funct3(Funct3),    // funct3 
+    Control u_ctrl(
+        .Op(opcode),  //opcode
+        .fun7(fun7),  //fun7 
+        .fun3(fun3),    // fun3 
         .RegWrite(RegWrite), // control signal for register write
         .MemWrite(MemWrite), // control signal for memory write
         .EXTOp(EXTOp),    // control signal to signed extension
         .ALUOp(ALUOp),    // ALU opertion
-        //output [2:0] NPCOp,    // next pc operation
+        .branch(branch),    // next pc operation
         .ALUSrc(ALUSrc),   // ALU source for b
         .DMType(DMType), //dm r/w type
-        .WDSel(WDSel)    // (register) write data selection  (MemtoReg)
+        .MemtoReg(MemtoReg)    // (register) write data selection  (MemtoReg)
     );
     //符号拓展
-    ext u_ext(
+    Imm_Gen u_ext(
         .iimm(iimm),  //instr[31:20], 12 bits
         .simm(simm), //instr[31:25, 11:7], 12 bits
+        .jimm(jimm), //20 bits
+        .bimm(bimm), //12 bits
         .EXTOp(EXTOp),
         .immout(immout)
     );
@@ -120,6 +159,7 @@ module SCPU_TOP(
     reg[4:0] reg_addr;
     
      //寄存器每周期顺序取数
+     //循环展示前10个寄存器的值
     always@(posedge Clk_CPU or negedge rstn)begin
         if (!rstn) begin reg_addr=5'b0;end 
         else if(sw_i[13] == 1'b1)begin
@@ -133,7 +173,7 @@ module SCPU_TOP(
     end
     
     //寄存器修改和取数
-    rf u_rf(
+    RF u_rf(
         .clk(Clk_CPU),
         .rstn(rstn),
         .RFWr(RegWrite),
@@ -152,17 +192,19 @@ module SCPU_TOP(
     wire signed[31:0] aluout;
 
     //alu_mux
-    assign A = RD1;
+    assign A = (branch == 2'b01)? rom_addr:RD1;
+    //assign A = RD1;
     assign B = (ALUSrc == 1'b1)?immout:RD2;
     //alu计算地址和算术式
-    alu u_alu(
+    ALU u_alu(
         .A(A),
         .B(B),
         .ALUOp(ALUOp),
         .C(aluout),
         .Zero(Zero)
     );
-      
+
+    
     reg [4:0] dmem_addr;
     reg [31:0]dmem_data;
     reg [31:0]dm_din;
@@ -183,7 +225,7 @@ module SCPU_TOP(
         end
     end
     //修改数据存储
-    dm u_dm(
+    DM u_dm(
         .clk(Clk_CPU),
         .DMWr(MemWrite),
         .addr(aluout),
@@ -194,13 +236,46 @@ module SCPU_TOP(
     
     //wd_mux
     always @(*) begin
-	   case(WDSel)
-		  `WDSel_FromALU: WD <= aluout;
-		  `WDSel_FromMEM: WD <= dm_out;
-		  //`WDSel_FromPC: WD<=PC_out+4;
+	   case(MemtoReg)
+		  `MemtoReg_FromALU: WD <= aluout;
+		  `MemtoReg_FromMEM: WD <= dm_out;
+		  `MemtoReg_FromPC: WD  <= rom_addr + 1;
 	   endcase
     end
-    
+    // 加法器
+    reg signed[31:0] temp;
+    always @(*) begin 
+        temp = immout;
+    end
+    wire signed[31:0] offset = rom_addr + (temp >>> 2);   
+    // 根据立即数与原有PC值相加的结果修改PC地址
+    // rom每周期地址顺序取数
+    always @(posedge Clk_CPU or negedge rstn) begin
+        if(!rstn) begin rom_addr = 5'b0; end
+        else begin
+            if (rom_addr < 7 & rom_addr >=0) begin
+                if(sw_i[1]==1'b0 & branch==2'b00)begin
+                    rom_addr = rom_addr + 1'b1;
+                end else if(branch == 2'b01) begin 
+                        // 是jal指令
+                        rom_addr = aluout;  
+                    end
+                    else if(branch == 2'b10) begin
+                        // 是beq指令
+                        if(Zero == 1'b1) begin 
+                            rom_addr = offset;
+                        end
+                        else rom_addr = rom_addr + 1'b1; 
+                    end
+                    else if(branch == 2'b11) begin
+                        // 是jalr指令
+                        rom_addr = aluout;
+                    end
+            end
+            else
+                rom_addr = 5'b00000; 
+            end
+        end
     reg[31:0] display_data;
     //显示模块数据选择
     always @(sw_i) begin
--- homework/vivado实验/DM.v
+++ homework/vivado实验/DM.v
@@ -1,61 +1,66 @@
 `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/12/02 12:35:48
+// Design Name: 
+// Module Name: dm
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 `define dm_word 3'b000
 `define dm_halfword 3'b001
-`define dm_halfword_unsigned 3'b010
 `define dm_byte 3'b011
-`define dm_byte_unsigned 3'b100
 
 module DM(
-input				clk,   //100MHZ CLK
-input 				DMWr,  // write signal
-input [5:0]			addr,  // 操作地址
-input [31:0]		din,   // 写入的数据
-input [2:0]			DMType,// 操作数据的类型 
-output reg [31:0]	dout   // 输出的数据，读取结果
+    input clk,  //100MHZ CLK
+    input DMWr,  //write signal
+    input [5:0]	addr,
+    input [31:0] din,
+    input [2:0]	DMType, 
+    output reg [31:0] dout 
     );
-reg[7:0] dmem[6:0];  // 初始化2的7次方(128)个字节的存储空间
-always @ (posedge clk) begin 
-    if(DMWr) begin
-        // write signal is valid
-        // 存储时不考虑符号扩展，只存数据
-        case(DMType)
-            `dm_byte:
-                dmem[addr] <= din[7:0];
-            `dm_halfword: begin
-                dmem[addr] <= din[7:0];
-                dmem[addr+1] <= din[15:8];
-            end
-            `dm_word: begin
-                dmem[addr] <= din[7:0];
-                dmem[addr+1] <= din[15:8];
-                dmem[addr+2] <= din[23:16];
-                dmem[addr+3] <= din[31:24];
-            end
-            default: begin
-                dmem[addr] <= din[7:0];
-                dmem[addr+1] <= din[15:8];
-                dmem[addr+2] <= din[23:16];
-                dmem[addr+3] <= din[31:24];
-            end
-        endcase 
-    end else  begin 
-        // null
-    end 
-    // read data
-    // 所有数据以补码形式存储和读取
-    case(DMType) 
-        `dm_byte: 
-            dout = {{24{dmem[addr][7]}},dmem[addr][7:0]};
-        `dm_byte_unsigned:
-            dout = {{24{1'b0}},dmem[addr][7:0]};
-        `dm_halfword:
-            dout = {{16{dmem[addr+1][7]}},dmem[addr+1][7:0],dmem[addr][7:0]};
-        `dm_halfword_unsigned:
-            dout = {{16{1'b0}},dmem[addr+1][7:0],dmem[addr][7:0]};
-        `dm_word:
-            dout = {dmem[addr+3][7:0],dmem[addr+2][7:0],dmem[addr+1][7:0],dmem[addr][7:0]};
-        default:
-            dout = {dmem[addr+3][7:0],dmem[addr+2][7:0],dmem[addr+1][7:0],dmem[addr][7:0]};
-    endcase
-end 
+    reg[7:0] dmem[127:0];
+    integer i = 0;
+    initial begin     
+        for(i = 0; i < 128; i = i + 1) begin
+            dmem[i] <= i;
+        end
+    end
+    
+    always@(posedge clk)begin
+        if(DMWr == 1)begin
+            case(DMType)//00字01半字11字节
+                `dm_byte:dmem[addr] <= din[7:0];
+                `dm_halfword:begin
+                    dmem[addr] <= din[7:0];
+                    dmem[addr+1] <= din[15:8];end
+                `dm_word:begin
+                    dmem[addr] <= din[7:0];
+                    dmem[addr+1] <= din[15:8];
+                    dmem[addr+2] <= din[23:16];
+                    dmem[addr+3] <= din[31:24];end
+            endcase
+        end
+    end
+    
+    always@(*)begin
+            case(DMType)//00字01半字11字节
+                `dm_byte:dout = {{24{dmem[addr][7]}},dmem[addr][7:0]};
+                `dm_halfword:dout = {{16{dmem[addr+1][7]}},dmem[addr+1][7:0],dmem[addr][7:0]};
+                `dm_word:dout = {dmem[addr+3][7:0],dmem[addr+2][7:0],dmem[addr+1][7:0],dmem[addr][7:0]};
+                default:dout = dout;
+            endcase
+    end
 endmodule
