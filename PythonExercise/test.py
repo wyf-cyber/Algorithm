b8c2549339a29ef95a1a2fb42a3ffb6337b177d7
wyf-cyber <wyf17326280236@gmail.com>
2024-01-31 18:09:25 +0800

    note

--- PythonExercise/test.py
+++ PythonExercise/test.py
@@ -1,16 +1,29 @@
-# 除了自上而下的计算，还可以自下而上地求出dp列表的值
-# 在自下而上的过程中，三角形每一层都只有一种节点，更为简单
-def f()->int:
-    n = int(input())
-    tri = []
-    for i in range(n):
-        tri.append([int(j) for j in input().split()])
-    # 算法主体
-    # 创建dp列表
-    dp = tri[:]
-    for row in range(n-2, -1, -1):
-        for col in range(row + 1):
-            dp[row][col] = min(dp[row+1][col], dp[row+1][col+1]) + tri[row][col]
-    return dp[0][0]
+def gcd(m, n)->int:
+    while m != n:
+        if m > n:
+            m = m - n
+        else:
+            n = n - m
+    return m
 
-print(f())
+# 求解多个数的最大公因数
+def multi_gcd(nums:list[int])->int:
+    n = len(nums)
+    if n == 1:
+        return nums[0]
+    elif n == 2:
+        return gcd(nums[0], nums[1])
+    else:
+        return gcd(multi_gcd(nums[:n//2]), multi_gcd(nums[n//2:]))
+
+n = int(input())
+nums = [int(i) for i in input().split()]
+nums = sorted(nums)
+# 求出新等差数列首尾项之间的差
+dist = nums[n-1] - nums[0]
+# 求出新等差数列的最大公差（给定元素的差的最大公因数）
+# 给定数字序列的最小差不一定就是等差数列的差，因为不同项在原等差数列中不一定有相等或成倍数的间距
+l = [nums[i] - nums[i-1] for i in range(1, n)]
+d = multi_gcd(l)
+print(d)
+print(dist//d + 1)
