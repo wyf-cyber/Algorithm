9f6878f7d5dcee7b9a9ad708e2195c2971654d30
wyf-cyber <wyf17326280236@gmail.com>
2024-02-04 10:16:00 +0800

    note

--- PythonExercise/test.py
+++ PythonExercise/test.py
@@ -1,29 +1,48 @@
-def gcd(m, n)->int:
-    while m != n:
-        if m > n:
-            m = m - n
-        else:
-            n = n - m
-    return m
+def multi(a,b):
+    # 将结果存入矩阵 c 并返回
+    c = []
+    rowA = len(a)
+    colA = len(a[0])
+    rowB = len(b)
+    colB = len(b[0])
+    # colA = rowB
+    # colC = colB
+    # rowC = rowA
+    for i in range(rowA):
+        line = []
+        for j in range(colB):
+            # 计算c[i][j]
+            temp = 0
+            for m in range(colA):
+                temp += a[i][m] * b[m][j]
+            line.append(temp)
+        c.append(line)
+    return c
 
-# 求解多个数的最大公因数
-def multi_gcd(nums:list[int])->int:
-    n = len(nums)
-    if n == 1:
-        return nums[0]
-    elif n == 2:
-        return gcd(nums[0], nums[1])
-    else:
-        return gcd(multi_gcd(nums[:n//2]), multi_gcd(nums[n//2:]))
+def powM(a,n):
+    if n == 0:
+        l = len(a)
+        for p in range(l):
+            for q in range(l):
+                if p == q:
+                    print('1', end = ' ')
+                else :
+                    print('0', end = ' ')
+            print()
+    ans = a
+    for i in range(n-1):
+        ans = multi(ans, a)
+    return ans
+def printM(a):
+    for i in a:
+        for j in i:
+            print(j, end = ' ')
+        print()
 
-n = int(input())
-nums = [int(i) for i in input().split()]
-nums = sorted(nums)
-# 求出新等差数列首尾项之间的差
-dist = nums[n-1] - nums[0]
-# 求出新等差数列的最大公差（给定元素的差的最大公因数）
-# 给定数字序列的最小差不一定就是等差数列的差，因为不同项在原等差数列中不一定有相等或成倍数的间距
-l = [nums[i] - nums[i-1] for i in range(1, n)]
-d = multi_gcd(l)
-print(d)
-print(dist//d + 1)
+# 接收输入
+n,m = map(int,input().split())
+b = []
+for i in range(n):
+    b.append([int(i) for i in input().split()])
+
+printM(powM(b,m))
